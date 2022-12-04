8c6f348ccd7b97d6fe3f7586ed401b4234907286
Ruveyda Cetin <ruveyda.cetin@d-teknoloji.com.tr>
2022-12-04 16:34:08 +0300

    decomposing url

---
+++ 02 Decomposing URLs/02 Decomposing URLs/02 Decomposing URLs/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Decomposing_URLs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string exampleUrl = "www.docs.microsoft.com/dotnet/csharp/whats-new/csharp-version-history";
+            var components = BreakdownURL(exampleUrl);
+            foreach (var item in components)
+            {
+                Console.WriteLine(item);
+                Console.ReadKey();
+            }
+        }
+
+        public static string[] BreakdownURL(string urlString)
+        {
+            //1 
+            string first = urlString.Remove(0, 4);
+
+            //2
+            string second = first.Replace("-", " ");
+
+            //3
+            string[] components = second.Split('/');
+
+            //Alternative ; 
+
+            string[] components2 = urlString.Remove(0, 4).Replace("-", " ").Split('/');
+
+
+            return components;
+        }
+    }
+}
